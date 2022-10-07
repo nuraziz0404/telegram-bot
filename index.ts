f7b9b24b1d4d26c483ee8646f80b9b8b9895c3e9
Aziz-cron <git@github.com>
2022-10-07 22:54:23 +0700

    add uptime

--- index.ts
+++ index.ts
@@ -4,9 +4,30 @@ import TelegramBot from "node-telegram-bot-api";
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
+function uptime() {
+  const t = Math.floor(process.uptime());
+  const _d = t % 86400;
+  const d = (t - _d) / 86400;
+
+  const _h = _d % 3600;
+  const h = (_d - _h) / 3600;
+
+  const _m = _h % 60;
+  const m = (_h - _m) / 60;
+
+  const s = _m;
+
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
+
 bot.getMe().then((e) => {
   console.log("[BOT] telegram bot started (https://t.me/%s)", e.username);
 });
+
+bot.onText(/\/uptime (.+)/i, (msg, match) => {
+  bot.sendMessage(msg.chat.id, `uptime: ${uptime()}`);
+});
+
 bot.on("message", (msg) => {
   console.log("[BOT] recheived msg: '%s'", msg.text);
   bot.sendMessage(msg.chat.id, "hello world");
