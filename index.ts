0a14ec4ebaaaa7f663a41d611ebc5b9a57fa80a8
Aziz-cron <git@github.com>
2022-10-08 20:25:38 +0700

    add exec and express

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 require("dotenv").config();
 import TelegramBot from "node-telegram-bot-api";
+import Express from "express";
+import { exec } from "child_process";
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
@@ -27,8 +29,28 @@ bot.getMe().then((e) => {
 bot.onText(/\/uptime(.*)/i, (msg, match) => {
   bot.sendMessage(msg.chat.id, `uptime: ${uptime()}`);
 });
+bot.onText(/\/exec(.*)/i, (msg, match) => {
+  console.log("[Exec] %s", match[1]);
+  exec(match[1], (err, out, outErr) => {
+    if (err || outErr) {
+      bot.sendMessage(msg.chat.id, `Exec error:\n` + String(err || outErr));
+    } else {
+      bot.sendMessage(msg.chat.id, `Exec output:\n` + String(out));
+    }
+  });
+});
 
 bot.on("message", (msg) => {
   console.log("[BOT] recheived msg: '%s'", msg.text);
   bot.sendMessage(msg.chat.id, "hello world");
+});
+
+const app = Express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(parseInt(process.env.PORT), "0.0.0.0", () => {
+  console.log("Express running on http://localhost:%s", process.env.PORT);
 });
